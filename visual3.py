c6d39630d97ccf2c5971d65fe03dbcf800fdbaf4
jbanford <152214680+jbanford@users.noreply.github.com>
2023-12-06 18:13:19 -0500

    visualization 3

--- visual3.py
+++ visual3.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_option('deprecation.showPyplotGlobalUse', False)
 import pandas as pd
 st.title("Visualizations III")
-df1 = pd.read_excel("Book2.xlsx",sheet_name='Sheet2')
+df1 = pd.read_excel("MGT4250 Course Project Data.xlsx",sheet_name='Sheet2')
 df2 = df1.melt(id_vars=['Year'], value_name='Genre')
 df_clean = df2.drop(columns=['variable'])
 col1, col2 = st.columns([0.5, 0.5])
@@ -26,3 +26,4 @@ plt.pie(grouped2['Count'], labels=grouped2['Genre'], autopct='%1.1f%%', startang
 plt.title(f"Genre Proportions for 2012-2022")
 plt.axis('equal')
 st.pyplot()
+
