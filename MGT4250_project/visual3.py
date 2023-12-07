ff53cac154a14255d8e94fac1e0b36454021eb26
jbanford <152214680+jbanford@users.noreply.github.com>
2023-12-07 13:33:17 -0500

    Update visual3.py

--- MGT4250_project/visual3.py
+++ MGT4250_project/visual3.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_option('deprecation.showPyplotGlobalUse', False)
 import pandas as pd
 st.title("Visualizations III")
-df1 = pd.read_excel("MGT4250 Course Project Data.xlsx",sheet_name='Sheet2')
+df1 = pd.read_excel("/Users/jacquelynbanford/Documents/MGT4250_project/MGT4250 Course Project Data.xlsx",sheet_name='Sheet2')
 df2 = df1.melt(id_vars=['Year'], value_name='Genre')
 df_clean = df2.drop(columns=['variable'])
 col1, col2 = st.columns([0.5, 0.5])
